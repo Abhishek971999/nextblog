7cf768b3a779315c46861aa26e737f71c6e01463
Abhishek Mogaveera <37222497+Abhishek971999@users.noreply.github.com>
2021-04-07 20:29:06 +0530

    init

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Next Blog + Typescript</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      
+    </>
+  )
+}
